72d15e76aae92fd0cde6870766003b25b495ebf4
Alex <zanaoriator@gmail.com>
2024-11-14 01:04:09 +0900

    Englisj

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://hobbito.github.io',
-  base: '/HeroDeveloper',
+  base: '/',
 });
